f3d3f18e6db569cc3b8e3313aa2364e1ae6bc0ac
sankhojjal <sankhojjalc@gmail.com>
2024-05-14 21:57:43 +0530

    slight modified memo function

--- memoization.js
+++ memoization.js
@@ -1,22 +1,23 @@
-const cache = {};
-const memoizedOne = (fn) => {
+const memoOne = (fn) => {
+  const obj = {}
   return (...args) => {
-    const key = args.join("-");
-    //if the params matches, then call from cache
-    if (cache[key]) {
-      console.log("i'm from cache");
-      return cache[key];
+    const key = args.join('-');
+        //if the params matches, then call from cache
+    if (obj.hasOwnProperty(key)) {
+      console.log('im from cache')
+      return obj[key]
     }
-    //else execute the function
+        //else execute the function
     const result = fn(...args);
-    cache[key] = result;
-    return result;
-  };
-};
+    obj[key]=result;
+    return obj[key]
+  }
+}
 const add = (a, b) => a + b;
+const memoAdd = memoOne(add);
 
-const memoizedAdd = memoizedOne(add);
-console.log(memoizedAdd(1, 2));
-console.log(memoizedAdd(1, 2));
-console.log(memoizedAdd(3, 4));
-console.log(memoizedAdd(1, 2));
+console.log(memoAdd(1, 2));
+console.log(memoAdd(3, 4));
+console.log(memoAdd(3, 2));
+console.log(memoAdd(1, 2));
+console.log(memoAdd(3, 4));
