97f02833093ca82a337220b737f2823348b191c0
sankhojjal <sankhojjalc@gmail.com>
2024-05-28 22:14:14 +0530

    auto-format

--- memoization.js
+++ memoization.js
@@ -1,18 +1,18 @@
 const memoOne = (fn) => {
-  const obj = {}
+  const obj = {};
   return (...args) => {
-    const key = args.join('-');
-        //if the params matches, then call from cache
+    const key = args.join("-");
+    //if the params matches, then call from cache
     if (obj.hasOwnProperty(key)) {
-      console.log('im from cache')
-      return obj[key]
+      console.log("im from cache");
+      return obj[key];
     }
-        //else execute the function
+    //else execute the function
     const result = fn(...args);
-    obj[key]=result;
-    return obj[key]
-  }
-}
+    obj[key] = result;
+    return obj[key];
+  };
+};
 const add = (a, b) => a + b;
 const memoAdd = memoOne(add);
 
