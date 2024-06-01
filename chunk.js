c0bd6a888447f734e275a911f325e51f49aca512
sankhojjal <sankhojjalc@gmail.com>
2024-06-01 20:49:11 +0530

    added chunks

---
+++ chunk.js
@@ -0,0 +1,13 @@
+const chunk = (arr, size) => {
+  const result = [];
+
+  for (let i = 0; i < arr.length; ) {
+    console.log(i);
+    result.push(arr.slice(i, i + size));
+    i += size;
+  }
+
+  return result;
+};
+
+console.log(chunk([1, 2, 3, 4, 5], 2));
