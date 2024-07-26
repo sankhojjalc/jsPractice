2fd5abf1904313456628c1bc1eda8bdf319dd5ba
sankhojjal <sankhojjalc@gmail.com>
2024-07-27 00:10:13 +0530

    added EPAM promise question

---
+++ promisePractice2.js
@@ -0,0 +1,18 @@
+async function asyncSum(promiseArr) {
+  let sum = 0;
+  for (let i = 0; i < promiseArr.length; i++) {
+    try {
+      sum += await promiseArr[i];
+    } catch (error) {
+      continue;
+    }
+  }
+  return sum;
+}
+
+asyncSum([Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)]).then(
+  (x) => console.log(x)
+); // 6
+asyncSum([Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]).then(
+  (x) => console.log(x)
+); // 4
