52168e837f959167773d7217c436c8fdb28a1d42
sankhojjal <sankhojjalc@gmail.com>
2024-07-28 21:58:00 +0530

    used logic of for of loop

--- promisePractice2.js
+++ promisePractice2.js
@@ -1,14 +1,26 @@
-async function asyncSum(promiseArr) {
+// async function asyncSum(promiseArr) {
+//   let sum = 0;
+//   for (let i = 0; i < promiseArr.length; i++) {
+//     try {
+//       sum += await promiseArr[i];
+//     } catch (error) {
+//       continue;
+//     }
+//   }
+//   return sum;
+// }
+
+const asyncSum = async (promiseArr) => {
   let sum = 0;
-  for (let i = 0; i < promiseArr.length; i++) {
+  for (item of promiseArr) {
     try {
-      sum += await promiseArr[i];
+      sum += await item;
     } catch (error) {
       continue;
     }
   }
   return sum;
-}
+};
 
 asyncSum([Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)]).then(
   (x) => console.log(x)
